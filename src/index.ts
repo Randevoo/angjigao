7eb6c19a0fced23979001c8cd459cf3a1d6d1200
WInson <winsonheng@hotmail.com>
2019-10-02 23:44:23 -0700

    Listen to heroku port

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ const startServer = async () => {
   });
 
   // The `listen` method launches a web server.
-  server.listen().then(({ url }) => {
+  server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
   });
 };
